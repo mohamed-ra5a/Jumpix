c9326bdd050483bbbd0546eb2bac5feee12d8986
mohamed-ra5a <mohamed.mahany.ra5a@gmail.com>
2026-03-22 01:37:35 +0200

    finishing player animation+ start working at tilemap

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,39 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const JUMP_VELOCITY = -850.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+	
+	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
+	
+	# Movement
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+	
+	# Flip
+	if direction == 1:
+		animated_sprite_2d.flip_h = false
+	elif direction == -1:
+		animated_sprite_2d.flip_h = true
+	
+	# Animations (IMPORTANT PART 🔥)
+	if not is_on_floor():
+		animated_sprite_2d.animation = "jumping"
+	elif abs(velocity.x) > 1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
